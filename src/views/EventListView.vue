c9245e622697ea7b4610b359fe8be228a39dd0fb
salah <eljettiouisalaheddine@gmail.com>
2023-04-08 14:57:32 +0000

    setting errors

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -2,34 +2,7 @@
 import EventCard from '../components/EventCard.vue'
 import { ref } from 'vue';
 const events = ref(
- [{
-  id: 5928101,
-  category: 'animal welfar',
-  title: 'Cat Luna',
-  time: '12:00',
-  date: 'Junuary 28, 2022'
-},
- {
-  id: 5101,
-  category: 'animal welfar',
-  title: ' Luna',
-  time: '12:00',
-  date: 'Junuary 28, 2022'
-},
- {
-  id: 5,
-  category: 'animal welfar',
-  title: 'Cat',
-  time: '12:00',
-  date: 'Junuary 28, 2022'
-},
- {
-  id: 5901,
-  category: 'animal welfar',
-  title: 'Cat Luna',
-  time: '12:00',
-  date: 'Junuary 28, 2022'
-}]
+ 
 )
 
 </script>
