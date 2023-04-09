0941813f34edabe9bdad0dcc79c70bb8ec353e38
salah <eljettiouisalaheddine@gmail.com>
2023-04-09 23:57:18 +0000

    styling paginations

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -41,19 +41,21 @@ export default {
 <template>
  <div class="events">
   <EventCard v-for="event in events" :key="event.id" :event="event" />
-  <RouterLink
-        id="page-prev"
-        :to="{ name: 'event-list', query: { page: page - 1 } }"
-        rel="prev"
-        v-if="page != 1"
-        >&#60; Previous</RouterLink>
-      <router-link
-        id="page-next"
-        :to="{ name: 'event-list', query: { page: page + 1 } }"
-        rel="next"
-        v-if="hasNextPage"
-        >&#60; Next</router-link
-      >
+  <div class="pagination">
+    <RouterLink
+    id="page-prev"
+    :to="{ name: 'event-list', query: { page: page - 1 } }"
+    rel="prev"
+    v-if="page != 1"
+    >&#60; Previous</RouterLink>
+    <router-link
+    id="page-next"
+    :to="{ name: 'event-list', query: { page: page + 1 } }"
+    rel="next"
+    v-if="hasNextPage"
+    > Next &#62;</router-link
+    >
+  </div>
  </div>
 </template>
 
@@ -62,5 +64,20 @@ export default {
    display: flex;
    flex-direction: column;
    align-items: center;
+  }
+  .pagination{
+    display: flex;
+    width: 290px;
+  }
+  .pagination a {
+    flex: 1;
+    text-decoration: none;
+    color: #2c3e50;
+  }
+  #page-prev{
+    text-align: left;
+  }
+  #page-next{
+    text-align: right;
   }
 </style>
